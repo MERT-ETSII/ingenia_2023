df3e300734c08acad3a365a6428f2d31bfdb0f6d
Juan <juanseguranunez@gmail.com>
2023-04-14 18:11:18 +0200

    Current sensor calibration

--- Ensayos/Prueba_ESP/src/main.cpp
+++ Ensayos/Prueba_ESP/src/main.cpp
@@ -11,12 +11,17 @@ float Sensibilidad=0.3; //sensibilidad en Voltios/Amperio para sensor de 5A
 
 float get_current(int n_muestras)
 {
+  // Corriente en amperios: (lectura analógica - 553.2731257)/31.17464354
+  // Asumiendo que la lectura analógica de (0-1023)
+  // Fuente: confía
   float voltajeSensor;
   float corriente=0;
   for(int i=0;i<n_muestras;i++)
   {
-    voltajeSensor = analogRead(CURRENTSENSOR) * (3 / 1023.0);////lectura del sensor
-    corriente=corriente+(voltajeSensor-1.5)/Sensibilidad; //Ecuación  para obtener la corriente
+    // voltajeSensor = analogRead(CURRENTSENSOR) * (3 / 1023.0);////lectura del sensor
+    // corriente=corriente+(voltajeSensor-1.5)/Sensibilidad; //Ecuación  para obtener la corriente
+    voltajeSensor = (analogRead(CURRENTSENSOR) - 553.2731257)/31.17464354;
+    corriente += voltajeSensor;
   }
   corriente=corriente/n_muestras;
   return(corriente);
@@ -26,6 +31,7 @@ float get_current(int n_muestras)
 void setup() {
   pinMode(MOTOR, OUTPUT);
   Serial.begin(9600);
+  Serial.println("############## BEGIN ##############");
 }
 void stop(){
   while (true){
@@ -37,15 +43,17 @@ void stop(){
 void loop() {
   
   float I=get_current(200);//obtenemos la corriente promedio de 200 muestras 
-  Serial.print("Corriente: ");
-  Serial.println(I,3); 
-  if(I < 0.3 || I > 0.4){
-    Serial.print("STOP");
+  if(I < -0.4 || I > 0.4){
+    Serial.print("STOP: ");
+    Serial.println(I);
     digitalWrite(MOTOR,LOW);
-    stop();
   }
-  else
-     digitalWrite(MOTOR,HIGH);
+  else{
+      Serial.print("Corriente: ");
+      Serial.println(I,3); 
+      digitalWrite(MOTOR,HIGH);
+  }
+     
 
   delay(500);
 }
