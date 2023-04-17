3c6210299a9d584849f36f533033ebebb9dea2d6
Juan <juanseguranunez@gmail.com>
2023-04-17 11:01:34 +0200

    Commented production code and rearranged everything

--- UC/include/classic_gripper.h
+++ UC/include/classic_gripper.h
@@ -1,3 +1,29 @@
-enum Error{
-    OK = 0
+enum class Error
+{
+    OK = 0,
+    TIMEOUT = -1
 };
+
+namespace cg
+{
+    unsigned long MAX_CLOSING_TIME_MILLIS = 3000;
+    int PIN_MOTOR = 4;
+    int PIN_CURRENTSENSOR = 0;
+
+    int CURRENT_THRESHOLD = 0.4;
+
+    //@brief Function to obtain current averaged over n samples
+    //@param[in] n_samples: number of iterations to use for current filter
+    float get_current(int n_samples);
+    
+    /*
+    * Function that performs the closing action. 
+    *
+    * This implies sending a signal to the motor to begin movement, 
+    * read the current sensor until it reaches certain threshold 
+    * (overcurrent means that the motor is blocked, so the object is picked) 
+    * and exiting. 
+    * If the action takes too long, an error is returned.
+    */
+    Error close_gripper();
+}
--- UC/src/main.cpp
+++ UC/src/main.cpp
@@ -6,14 +6,66 @@
  * 
 */
 
-
 #include <Arduino.h>
+#include<math.h>
 #include "classic_gripper.h"
 
-void setup() {
-  // put your setup code here, to run once:
+
+float cg::get_current(int n_samples)
+{
+  // Current in Amperes: (analog reading - 553.2731257)/31.17464354
+  // for an analog reading in range 0 - 1023
+  // Source: trust me, bro
+
+  float voltage_sensor;
+  float current = 0;
+  for(int i=0;i<n_samples;i++)
+  {
+    voltage_sensor = (analogRead(cg::PIN_CURRENTSENSOR) - 553.2731257)/31.17464354;
+    current += voltage_sensor;
+  }
+  current = current/n_samples;
+  return(current);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+Error cg::close_gripper()
+{
+  float I=cg::get_current(200); //Read current over 200 samples
+
+  // TO DO: Don't use delays
+  digitalWrite(cg::PIN_MOTOR, HIGH);  // Run motor 500 ms
+  delay(500);
+
+  unsigned long init_time = millis();
+  while(fabs(I) < cg::CURRENT_THRESHOLD) // Still moving
+  {
+    // If operation takes too long, there is probably an error. 
+    // Stop the motor and exit
+    if(millis() - init_time > cg::MAX_CLOSING_TIME_MILLIS)
+      {
+        digitalWrite(cg::PIN_MOTOR, LOW);  // Stop motor
+        return Error::TIMEOUT;
+      }
+  }
+
+  digitalWrite(cg::PIN_MOTOR, LOW);  // Stop motor
+  return Error::OK;
+}
+
+void setup() 
+{
+  /******* Declaration of pin modes *******/
+
+  // Classic Gripper
+  pinMode(cg::PIN_CURRENTSENSOR, INPUT);
+  pinMode(cg::PIN_MOTOR, OUTPUT);
+
+  /***************************************/
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  cg::close_gripper();
 }
