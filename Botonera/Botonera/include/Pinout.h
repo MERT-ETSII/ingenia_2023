5e045fe5c9d3af79c491ea4b698a8dc07d393ebf
adri5104 <92983875+adri5104@users.noreply.github.com>
2023-05-04 11:29:59 +0200

    ruteo conectores esp32

--- Botonera/Botonera/include/Pinout.h
+++ Botonera/Botonera/include/Pinout.h
@@ -11,21 +11,21 @@ const uint8_t PIN_LCD_BACKLIGHT = 3;
 const uint8_t ADDRESS_LCD = 0x38;
 
 // Botones
-const uint8_t PIN_BOTON_LLAMAR = 4;
-const uint8_t PIN_BOTON_ENVIAR = 12;
-const uint8_t PIN_BOTON_REARME = 13;
-const uint8_t PIN_BOTON_STOP = 23;
+const uint8_t PIN_BOTON_LLAMAR = 14;
+const uint8_t PIN_BOTON_ENVIAR = 27;
+const uint8_t PIN_BOTON_REARME = 26;
+const uint8_t PIN_BOTON_STOP = 25;
 
 // Leds 
-const uint8_t PIN_LED_ENVIAR = 2;
-const uint8_t PIN_LED_LLAMAR = 18;
-const uint8_t PIN_LED_REARME = 19;
+const uint8_t PIN_LED_ENVIAR = 33;
+const uint8_t PIN_LED_LLAMAR = 32;
+const uint8_t PIN_LED_REARME = 35;
 
 
 
 // Teclado
 // https://arduinogetstarted.com/tutorials/arduino-keypad
-byte pin_rows[ROW_NUM] = {14,15,16,17}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {34,35,36,39}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {15,2,4,16}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {17,5,18,19}; //connect to the column pinouts of the keypad
 
 #endif
